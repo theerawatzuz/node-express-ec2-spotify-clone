39bb8774b2a2fec1d377132fd124ee03864c97f7
silimechok <theerawat.silimechok@accenture.com>
2024-04-09 20:39:55 +0700

    fixBug

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import express from 'express'
 
 const app = express()
 
-app.lisstem(5001, () => console.log('Api is runnung on port 5001'))
+app.listen(5001, () => console.log('Api is runnung on port 5001'))
 
 app.get('/', (req, res) => res.json('My API is running'))
